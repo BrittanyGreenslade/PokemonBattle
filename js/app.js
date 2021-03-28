a2d37d93287cedcb808ed877f0326a7899d0048c
Brittany <bgreensl@ualberta.ca>
2021-03-27 19:42:15 -0600

    lots of JS done

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 function setPokemon(selectedPokemon) {
   Cookies.set("selection", selectedPokemon);
+  Cookies.set("userCurrentHealth", `400`);
+  Cookies.set("userMaxHealth", `400`);
+  Cookies.set("compCurrentHealth", `400`);
+  Cookies.set("compMaxHealth", `400`);
   window.open(`/pages/secondPage.html`, `_self`);
 }
--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,8 @@
 var getPokemon = Cookies.get("selection");
-// console.log(getPokemon);
-
+var gameOver = false;
+// if gameOver = false;
+// else
+//but need to check if game is over before this code runs
 var playerImg = document.getElementById("player");
 if (getPokemon === `Poliwhirl`) {
   playerImg.innerHTML = `<img src="/images/Poliwhirl.gif" alt="Poliwhirl gif" />`;
@@ -9,3 +11,67 @@ if (getPokemon === `Poliwhirl`) {
 } else if (getPokemon === `Snorlax`) {
   playerImg.innerHTML = `<img src="/images/Snorlax.gif" alt="Snorlax gif" />`;
 }
+
+var compMaxHealth = Cookies.get("compMaxHealth", compMaxHealth);
+document.getElementById(
+  `healthPointsP2`
+).innerText = `Player 2 Health: ${compMaxHealth}`;
+var userMaxHealth = Cookies.get("userMaxHealth", userMaxHealth);
+document.getElementById(
+  `healthPointsP1`
+).innerText = `Player 1 Health: ${userMaxHealth}`;
+
+var compCurrentHealth = Cookies.get("compCurrentHealth");
+var userCurrentHealth = Cookies.get("userCurrentHealth");
+
+function takeTurn(damage) {
+  cpuCurrentHealth = compCurrentHealth - damage;
+  usrCurrentHealth = userCurrentHealth - damage;
+  document.getElementById(
+    `healthPointsP2`
+  ).innerText = `Player 2 Health: ${cpuCurrentHealth}`;
+  document.getElementById(
+    `healthPointsP1`
+  ).innerText = `Player 1 Health: ${usrCurrentHealth}`;
+  if (gameOver) {
+    if (cpuCurrentHealth > 1 && usrCurrentHealth > 1) {
+    }
+  }
+  if ((gameOver = true)) {
+    // if (compCurrentHealth <= 1 && userCurrentHealth <= 1) {
+    // if (userCurrentHealth > 49) {
+    //   document.getElementById(
+    //     `healthPointsP2`
+    //   ).innerText = `Player 2 Health: ${userCurrentHealth}`;
+    // }
+    // && userCurrentHealth >
+    // && compCurrentHealth > 50
+    if (cpuCurrentHealth <= 49) {
+      document.getElementById(
+        "winContainer"
+      ).innerHTML = `<h2>You win ${getPokemon}!</h2>`;
+      // compCurrentHealth = 0;
+    } else if (usrCurrentHealth <= 49) {
+      document.getElementById(
+        "winContainer"
+      ).innerHTML = `<h2>You win Blissey!</h2>`;
+      // userCurrentHealth = 0;
+    } else if (usrCurrentHealth < 49 && cpuCurrentHealth < 49) {
+      document.getElementById(
+        "winContainer"
+      ).innerHTML = `<h2>You win ${getPokemon}</h2>`;
+      usrCurrentHealth = 1;
+      cpuCurrentHealth = 0;
+    }
+
+    Cookies.set("compCurrentHealth", cpuCurrentHealth);
+    Cookies.set("userCurrentHealth", usrCurrentHealth);
+  }
+}
+
+// console.log(compCurrentHealth);
+// console.log(userCurrentHealth);
+
+// Cookies.set ("gameProgress",)
+
+//Object.keys() to count number of key-value pairs in an OBJECT (.length for arrays)
